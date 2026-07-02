adf1ba26ea574d85bcd79739b75d9186b9de8363
qayim <qayim00@gmail.com>
2026-07-02 16:11:12 +0800

    Add new background image (background7.png) to assets

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ const baseUrl = import.meta.env.BASE_URL.replace(/\/$/, '');
 
 document.documentElement.style.setProperty(
   '--page-background-image',
-  `url("${import.meta.env.BASE_URL}assets/background4.png")`
+  `url("${import.meta.env.BASE_URL}assets/background7.png")`
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
